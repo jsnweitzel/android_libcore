031b7e92da816e7e236495db60b401155f00a02b
Elliott Hughes <enh@google.com>
2011-07-07 13:47:33 -0700

    Offer default implementations of various dalvik intrinsics in the library.
    (cherry-picked from commit 2f95766aac3df74c6c641232eec4791b2330c9df)
    
    Change-Id: I1005ee22ed8a017bb536bfd76df2173c7e8dcf64

--- luni/src/main/native/sub.mk
+++ luni/src/main/native/sub.mk
@@ -14,6 +14,8 @@ LOCAL_SRC_FILES := \
 	java_io_File.cpp \
 	java_io_ObjectStreamClass.cpp \
 	java_lang_Character.cpp \
+	java_lang_Double.cpp \
+	java_lang_Float.cpp \
 	java_lang_Math.cpp \
 	java_lang_ProcessManager.cpp \
 	java_lang_RealToString.cpp \
